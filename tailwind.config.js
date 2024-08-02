4dbe41f02c0d86fab6baf1a3d4c6364404a40848
Christian Cordeiro <christiancordeiro96@gmail.com>
2024-08-02 17:03:13 -0300

    font correction

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        CodeSquared: ["Code Squared"],
+        CodeSquared: ["Code-Squared"],
         CallingCode: ["CallingCode-Regular"],
       },
     },
